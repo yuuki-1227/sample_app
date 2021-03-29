d5ef26f40259d55715616b813df2e92a60c29e15
yuuki-1227 <uncle.ozin.ochiai@gmail.com>
2021-03-29 06:41:21 +0000

    [Start]2-8

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   get 'todolists' => 'todolists#index'
   get 'todolists/:id' => 'todolists#show', as:'todolist'
   get 'todolists/:id/edit' => 'todolists#edit', as: 'edit_todolist'
-  patch 'todolists/:id' => 'todolists#update', ad: 'update_todolist'
+  patch 'todolists/:id' => 'todolists#update', as: 'update_todolist'
+  delete 'todolists/:id' => 'todolists#destory', as: 'destory_todolist'
   end
 
