56f8b9b8c879ca8ccb7d845b0225e1f21cf06cfe
yuuki-1227 <uncle.ozin.ochiai@gmail.com>
2021-03-29 07:11:40 +0000

    [Update]2-8

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -33,6 +33,12 @@ class TodolistsController < ApplicationController
     redirect_to todolist_path(list.id)
   end
 
+  def destroy
+    list = List.find(params[:id])
+    list.destroy
+    redirect_to todolists_path
+  end
+
   private
   # ストロングパラメータ
   def list_params
