e9cfe3bda62b2858229b551fe970e08cb98baa1b
yuuki-1227 <uncle.ozin.ochiai@gmail.com>
2021-03-29 02:50:12 +0000

    [start]2-5

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'top' => 'homes#top'
   post 'todolists' => 'todolists#create'
+  get 'todolists' => 'todolists#index'
+
   end
-  
+
