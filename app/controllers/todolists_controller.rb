2aaba8c975eadb1666b7ed8b0a2c3b81d226e1b9
yuuki-1227 <uncle.ozin.ochiai@gmail.com>
2021-03-29 02:09:35 +0000

    progress1

---
+++ app/controllers/todolists_controller.rb
@@ -0,0 +1,20 @@
+class TodolistsController < ApplicationController
+  def new
+    @list = List.new
+  end
+
+  def create
+    #1.データを新規登録するためのインスタンス作成
+  　list = List.new(list_params)
+  　#2.saveメソッド作成
+  　list.save
+  　#3.トップ画面にリダイレクト
+  　redirect_to '/top'
+  end
+
+  private
+  #ストロングパラメータ
+  　def list_params
+  　  params.require(:list).permit(:title,:body)
+  　end
+end
